ebc49ca6dd3366f20365c828491a3915856dfddf
tejavictory <s533708@nwmissouri.edu>
2018-11-18 19:59:10 -0600

    Commited with project files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # 3DModellingTutorial
-WebApps Tutorial
+WebApps 
+Required:
+Autodesk MAYA
+Three.js libraries
+OBJ and MTL loader
+Orbit Controls
+
+Implementation:
+1. Create a 3D model in MAYA
+2. Export the model in OBJ format
+3. Load the model in three.js
+4. Run the server.
---
+++ public/examples/3d-obj-loader/scripts.js
@@ -0,0 +1,64 @@
+var scene = new THREE.Scene();
+scene.background = new THREE.Color( 0x000000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 200;
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
+
+
+var keyLight =  new THREE.DirectionalLight(new THREE.Color('hsl(0, 0%, 100%)'), 0.75);
+keyLight.position.set(-100, 0, 100);
+
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(0, 0%, 100%)'), 0.75);
+fillLight.position.set(100, 0, 100);
+
+var backLight = new THREE.DirectionalLight(new THREE.Color('hsl(0, 0%, 100%)'), 0.75);
+backLight.position.set(100, 0, -100).normalize();
+
+
+/* var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+keyLight.position.set(-100, 0, 100);
+
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+fillLight.position.set(100, 0, 100);
+
+var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+backLight.position.set(100, 0, -100).normalize(); */
+
+scene.add(keyLight);
+scene.add(fillLight);
+scene.add(backLight);
+
+var mtlLoader = new THREE.MTLLoader();
+mtlLoader.setTexturePath('/examples/3d-obj-loader/assets/');
+mtlLoader.setPath('/examples/3d-obj-loader/assets/');
+mtlLoader.load('Miami_2525.mtl', function (materials) {
+
+    materials.preload();
+
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.setPath('/examples/3d-obj-loader/assets/');
+    objLoader.load('Miami 2525.obj', function (object) {
+
+        scene.add(object);
+        object.position.y -= 60;
+
+    });
+
+});
+
+var animate = function () {
+	requestAnimationFrame( animate );
+	controls.update();
+	renderer.render(scene, camera);
+};
+
+animate();
